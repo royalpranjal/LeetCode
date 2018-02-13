31dc8d628574123e0e26d5b812949f85d03625cb
Pranjal <ug201314013@iitj.ac.in>
2018-02-13 16:54:52 +0530

    Update K-th Symbol in Grammar.cpp

--- Algorithms/K-th Symbol in Grammar.cpp
+++ Algorithms/K-th Symbol in Grammar.cpp
@@ -1,26 +1,5 @@
 class Solution {
 public:
-    void dfs(int currValue, int row, long int& count, int N, int K, int& ans){
-        if(row == N || count >= K){
-            if(count == K){
-                ans = currValue;    
-                count++;
-            }
-            else{
-                return;    
-            }
-        }
-        
-        if(!currValue){
-            dfs(0, row+1, count, N, K, ans);
-            dfs(1, row+1, count, N, K, ans);
-        }
-        else{
-            dfs(1, row+1, count, N, K, ans);
-            dfs(0, row+1, count, N, K, ans);
-        }
-    }
-    
     int kthGrammar(int N, int K) {
 //         Exponential time (really bad algorithm)
 //         int ans = 0;
@@ -29,16 +8,8 @@ public:
         
 //         return ans;
         
-        // cout << "N: " << N << ", K: " << K << endl;
-        
-        if(K == 1){
+        if(N == 1){
             return 0;
-        }
-        else if(N == 1){
-            return 0;
-        }
-        else if(N == 2){
-            return 1;
         }
         else if(K > pow(2, N-2)){
             int diff = K - pow(2, N-2) - 1;
@@ -51,4 +22,25 @@ public:
         return kthGrammar(N-1, K);
         
     }
+//     void dfs(int currValue, int row, long int& count, int N, int K, int& ans){
+//         if(row == N || count >= K){
+//             if(count == K){
+//                 ans = currValue;    
+//                 count++;
+//             }
+//             else{
+//                 return;    
+//             }
+//         }
+        
+//         if(!currValue){
+//             dfs(0, row+1, count, N, K, ans);
+//             dfs(1, row+1, count, N, K, ans);
+//         }
+//         else{
+//             dfs(1, row+1, count, N, K, ans);
+//             dfs(0, row+1, count, N, K, ans);
+//         }
+//     }
+    
 };
